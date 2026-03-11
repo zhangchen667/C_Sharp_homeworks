fb8dc71019262c56fea11058e52845401a55530b
zhangchen667 <2271875988@qq.com>
2026-03-11 21:26:09 +0800

    第一次作业

---
+++ 第1次作业/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好，我是张宸豪");
+        }
+    }
+}
---
+++ 第1次作业/求素数/求素数/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace findsu
+{
+    class Program
+    {
+        public static bool isSu(int n)
+        {
+            for (int i = 2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("素数查找");
+            Console.WriteLine("请输入一个上限和一个下限：");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int count = 0;
+            for(int i = n1; i < n2; i++)
+            {
+                if (count == 10)
+                {
+                    count = 0;
+                    Console.WriteLine();
+                }
+                if (isSu(i))
+                {
+                    count++;
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+            }
+            
+        }
+    }
+}
