76b583f32a91bb09fa9c1f71fdca387be2ba8e23
暴食大王 <16917090+gluttony-king@user.noreply.gitee.com>
2026-04-17 00:33:41 +0800

    第六次作业

---
+++ 第6次作业/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstPhones = new System.Windows.Forms.ListBox();
+            this.lstEmails = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(39, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtUrl.Location = new System.Drawing.Point(103, 18);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(706, 34);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "https://";
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFetch.Location = new System.Drawing.Point(836, 16);
+            this.btnFetch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(154, 32);
+            this.btnFetch.TabIndex = 2;
+            this.btnFetch.Text = "获取并提取";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(39, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "手机号码：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(540, 64);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "邮箱：";
+            // 
+            // lstPhones
+            // 
+            this.lstPhones.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPhones.FormattingEnabled = true;
+            this.lstPhones.HorizontalScrollbar = true;
+            this.lstPhones.ItemHeight = 23;
+            this.lstPhones.Location = new System.Drawing.Point(39, 90);
+            this.lstPhones.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.lstPhones.Name = "lstPhones";
+            this.lstPhones.Size = new System.Drawing.Size(449, 280);
+            this.lstPhones.TabIndex = 5;
+            // 
+            // lstEmails
+            // 
+            this.lstEmails.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstEmails.FormattingEnabled = true;
+            this.lstEmails.HorizontalScrollbar = true;
+            this.lstEmails.ItemHeight = 23;
+            this.lstEmails.Location = new System.Drawing.Point(540, 90);
+            this.lstEmails.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.lstEmails.Name = "lstEmails";
+            this.lstEmails.Size = new System.Drawing.Size(449, 280);
+            this.lstEmails.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 424);
+            this.Controls.Add(this.lstEmails);
+            this.Controls.Add(this.lstPhones);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFetch);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "网页信息提取工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstPhones;
+        private System.Windows.Forms.ListBox lstEmails;
+
+        #endregion
+    }
+}
+
---
+++ 第6次作业/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        // 中国手机号正则表达式：1开头，第二位3-9，共11位
+        private static readonly Regex PhoneRegex = new Regex(
+            @"1[3-9]\d{9}",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase
+        );
+
+        // 邮箱正则表达式
+        private static readonly Regex EmailRegex = new Regex(
+            @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnFetch_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(url) || url == "https://")
+            {
+                MessageBox.Show("请输入有效的URL地址！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 确保URL有协议前缀
+            if (!url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) &&
+                !url.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            {
+                url = "http://" + url;
+                txtUrl.Text = url;
+            }
+
+            btnFetch.Enabled = false;
+            btnFetch.Text = "获取中...";
+            lstPhones.Items.Clear();
+            lstEmails.Items.Clear();
+
+            try
+            {
+                // 获取网页内容
+                string htmlContent = await FetchWebContent(url);
+
+                if (string.IsNullOrEmpty(htmlContent))
+                {
+                    MessageBox.Show("未能获取到网页内容，请检查URL是否正确！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // 提取手机号码
+                ExtractPhones(htmlContent);
+
+                // 提取邮箱
+                ExtractEmails(htmlContent);
+
+                // 显示统计信息
+                string message = $"提取完成！\n手机号码：{lstPhones.Items.Count} 个\n邮箱：{lstEmails.Items.Count} 个";
+                MessageBox.Show(message, "结果", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"发生错误：{ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnFetch.Enabled = true;
+                btnFetch.Text = "获取并提取";
+            }
+        }
+
+        private async Task<string> FetchWebContent(string url)
+        {
+            try
+            {
+                // 设置用户代理，避免某些网站拒绝请求
+                httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36");
+
+                // 获取网页内容
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                // 读取内容
+                string content = await response.Content.ReadAsStringAsync();
+
+                // 简单的HTML解码，处理&nbsp;等实体
+                content = System.Net.WebUtility.HtmlDecode(content);
+
+                return content;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void ExtractPhones(string content)
+        {
+            MatchCollection matches = PhoneRegex.Matches(content);
+
+            // 使用HashSet去重
+            HashSet<string> uniquePhones = new HashSet<string>();
+
+            foreach (Match match in matches)
+            {
+                string phone = match.Value;
+                uniquePhones.Add(phone);
+            }
+
+            foreach (string phone in uniquePhones)
+            {
+                lstPhones.Items.Add(FormatPhoneNumber(phone));
+            }
+        }
+
+        private void ExtractEmails(string content)
+        {
+            MatchCollection matches = EmailRegex.Matches(content);
+
+            // 使用HashSet去重
+            HashSet<string> uniqueEmails = new HashSet<string>();
+
+            foreach (Match match in matches)
+            {
+                string email = match.Value;
+                uniqueEmails.Add(email);
+            }
+
+            foreach (string email in uniqueEmails.OrderBy(e => e))
+            {
+                lstEmails.Items.Add(email);
+            }
+        }
+
+        private string FormatPhoneNumber(string phone)
+        {
+            // 格式化手机号：13812345678 -> 138 1234 5678
+            if (phone.Length == 11)
+            {
+                return $"{phone.Substring(0, 3)} {phone.Substring(3, 4)} {phone.Substring(7, 4)}";
+            }
+            return phone;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
